49c26655d8e63714f552acc99a5dfee7a61910d2
yuka-52 <98066974+yuka-52@users.noreply.github.com>
2022-02-15 15:50:40 +0900

    js課題

---
+++ calc.js
@@ -0,0 +1,15 @@
+'use strict';
+
+
+ function get_calc(btn){
+      let result = document.getElementById("result")
+      let btn_value = btn.innerHTML;
+     
+      if (btn_value == "=") {
+       console.log("=")
+     } else if (btn_value == "="){
+       console.log("=")
+     } else {
+       console.log("それ以外")
+     }
+     }
